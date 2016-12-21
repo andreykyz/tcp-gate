721018fbd0a05e76fd31c6aeb40532a9c31e97c3
Кузнецов Андрей Андреевич <a.a.kuznetsov@acti.ru>
2016-12-21 17:46:34 +0300

    queue listener compleate

--- queue.go
+++ queue.go
@@ -6,21 +6,58 @@ type ParcedPacket struct {
 	data      []byte
 }
 
+/*
+PacketQ is a packet queue fo sequentially data flushing
+nextPacketKey = seqNum + len
+*/
 type PacketQ struct {
-	in    chan ParcedPacket
-	out   chan []byte
-	queue map[int32]*ParcedPacket
+	in            chan *ParcedPacket
+	out           chan []byte
+	done          chan bool
+	queue         map[uint32]*ParcedPacket // map key is packet seqNum
+	minNextSeqNum uint32                   //is a minimal next seq num seqNum + len
 }
 
 func newPacketQ() (packetQ *PacketQ) {
 
 	packetQ = &PacketQ{}
-	packetQ.in = make(chan ParcedPacket, 100)
+	packetQ.in = make(chan *ParcedPacket, 100)
 	packetQ.out = make(chan []byte, 100)
 
 	return packetQ
 }
 
-func (packetQ *PacketQ) Listener {
+func (packetQ *PacketQ) QueueListener() {
+	for {
+		exit := false
+		select {
+		case <-packetQ.done:
+			exit = true
+			break
+		case packet := <-packetQ.in:
+			if packet.tcpHeader.SeqNum == packetQ.minNextSeqNum {
+				packetQ.out <- packet.data
+				packetQ.minNextSeqNum = packet.tcpHeader.SeqNum + uint32(len(packet.data))
+			} else {
+				packetQ.queue[packet.tcpHeader.SeqNum] = packet
+			}
 
+			for {
+				ok := true
+				if packet, ok = packetQ.queue[packetQ.minNextSeqNum]; !ok {
+					break
+				}
+				packetQ.out <- packet.data
+				packetQ.minNextSeqNum = packet.tcpHeader.SeqNum + uint32(len(packet.data))
+				delete(packetQ.queue, packetQ.minNextSeqNum)
+			}
+		}
+		if exit {
+			break
+		}
+	}
+}
+
+func (packetQ *PacketQ) addPacket(packet *ParcedPacket) {
+	packetQ.in <- packet
 }
